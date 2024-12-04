5a5ef633c589402d4625155fc796436dd44be8d7
MrSharma003 <sprashant145.ms@gmail.com>
2024-12-04 13:44:42 +0530

    add

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // Update with your backend base URL
+  baseURL: "https://restaurant-backend-two.vercel.app/", // Update with your backend base URL
 });
 
 export const registerRestaurant = async (data: any) => {
